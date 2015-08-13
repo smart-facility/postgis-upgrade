bbc6988d0cadd2b2c7494a22701ac802582bad5b
Matthew Berryman <mberryma@uow.edu.au>
2015-08-13 10:35:01 +1000

    Remove support for .custom files
    Have changed backup scripts to use .backup consistent with our other
    code, so this code can change too.

--- upgrade_and_restore.sh
+++ upgrade_and_restore.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
-FILES=*.custom
-for f in $FILES
-do
-	f=`basename $f .custom`
-	createdb -h localhost -U postgres $f
-	psql -h localhost -U postgres -f /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql $f
-	psql -h localhost -U postgres -f /usr/share/postgresql/9.3/contrib/postgis-2.1/topology.sql $f
-	psql -h localhost -U postgres -f /usr/share/postgresql/9.3/contrib/postgis-2.1/rtpostgis.sql $f
-	/usr/share/postgresql/9.3/contrib/postgis-2.1/postgis_restore.pl $f.custom | psql -h localhost -U postgres $f
-	psql -h localhost -U postgres -f asbinary.sql $f
-done
-
 FILES=*.backup
 for f in $FILES
 do
