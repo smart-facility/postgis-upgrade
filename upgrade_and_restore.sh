db198c0112071d58ee4d4ef2fb61fa5883c625ce
Matthew Berryman <mberryma@uow.edu.au>
2015-08-13 12:57:16 +1000

    Fix extension in call to postgis_restore.pl

--- upgrade_and_restore.sh
+++ upgrade_and_restore.sh
@@ -7,6 +7,6 @@ do
 	psql -h localhost -U postgres -f /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql $f
 	psql -h localhost -U postgres -f /usr/share/postgresql/9.3/contrib/postgis-2.1/topology.sql $f
 	psql -h localhost -U postgres -f /usr/share/postgresql/9.3/contrib/postgis-2.1/rtpostgis.sql $f
-	/usr/share/postgresql/9.3/contrib/postgis-2.1/postgis_restore.pl $f.custom | psql -h localhost -U postgres $f
+	/usr/share/postgresql/9.3/contrib/postgis-2.1/postgis_restore.pl $f.backup | psql -h localhost -U postgres $f
 	psql -h localhost -U postgres -f asbinary.sql $f
 done
